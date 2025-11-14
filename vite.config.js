36f21b1c5d21f8695240d62a1ff03fe47076d070
keerthikaa27 <keerthikanimmagadda@gmail.com>
2025-11-14 23:13:22 +0530

    Fixed errors

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/Portfolio/",  // <-- important for GitHub Pages
+  base: '/Portfolio/',  
   plugins: [react()],
+  build: {
+    outDir: 'dist', 
+    assetsDir: 'assets',
+  },
 });
