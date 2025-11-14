9ee0415debe5923eb2bdfbfaab50c4abe8c58bdb
keerthikaa27 <keerthikanimmagadda@gmail.com>
2025-11-14 15:01:55 +0530

    Describe what you changed

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
-// src/main.jsx
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css"; // 👈 only this; remove styles.css for now
+import "./index.css"; 
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
